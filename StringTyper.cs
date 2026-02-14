717b7c831e6c15d145ba92deee0cf729e469f836
forestoak777 <forestoak777@gmail.com>
2026-02-14 13:02:35 -0500

    initial commit please work

---
+++ StringTyper.cs
@@ -0,0 +1,31 @@
+using System.Reflection.Metadata.Ecma335;
+using System.Threading;
+
+public static class StringTyper
+{
+
+    public static void TypeString(string str = "", int milisecondsToSleep = 10)
+    {
+        foreach (char c in str)
+        {
+            Console.Write(c);
+            Thread.Sleep(milisecondsToSleep);
+        }
+        Console.WriteLine();
+    }
+
+    public static void PoopString(string str = "", int milisecondsDelay = 100)
+    {
+        
+        Thread.Sleep(milisecondsDelay);
+        Console.WriteLine(str);
+    }
+
+    public static void PoopDelay(int milisecondsDelay = 100)
+    {
+        Thread.Sleep(milisecondsDelay);
+    }
+    
+    
+
+}
